3b6e01561c453a4398a033472d86a66b43d5b6fc
Ohad Wolfman <ohad1696@gmail.com>
2023-05-17 08:17:46 +0300

    # 2 commit

--- ex2_utils.py
+++ ex2_utils.py
@@ -17,8 +17,14 @@ def conv1D(in_signal: np.ndarray, k_size: np.ndarray) -> np.ndarray:
     :param k_size: 1-D array as a kernel
     :return: The convolved array
     """
-
-    return
+    # We will use the function convolve from numpy, that get 3 parameters:
+    # a - First 1D input array,
+    # v - Second 1D input array,
+    # Mode - 3 options:
+    #   {‘full’ - output shape: (a+v-1 , 1),
+    #   ‘valid’ - output shape: max(a, v) - min(a, v) + 1,
+    #   ‘same’ - output shape: Max(a,v) }
+    return np.convolve(in_signal,k_size,"full")
 
 
 def conv2D(in_image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
@@ -28,8 +34,17 @@ def conv2D(in_image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
     :param kernel: A kernel
     :return: The convolved image
     """
-
-    return
+    # We will use the function filter2D from cv2, that get the parameters:
+    # src – input image.
+    # ddepth – desired depth of the dst image, -1 means the same as src.depth()
+    # kernel - the kernel, a single-channel floating point matrix
+    # dst – output image of the same size and the same number of channels as src.
+    # anchor – indicates the relative position of a filtered point within the kernel, (-1, -1) is the kernel center
+    # delta – optional value added to the filtered pixels before storing them in dst.
+    # borderType – pixel extrapolation method
+    return cv2.filter2D(src=in_image, ddepth=-1, kernel=kernel,
+                        dst=None, anchor=(-1, -1), delta=0, borderType=cv2.BORDER_DEFAULT)
+    # we could write only:     convolved_image = cv2.filter2D(in_image, -1, kernel)
 
 
 def convDerivative(in_image: np.ndarray) -> (np.ndarray, np.ndarray):
